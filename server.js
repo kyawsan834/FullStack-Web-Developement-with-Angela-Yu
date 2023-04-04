29cf67de8d9f9ab28f7b6499ad17773f238569e4
kyawsan84yay <kyawsan84yay@gmail.com>
2023-04-04 13:01:21 +0630

    4/4/2023 1:00PM

---
+++ server.js
@@ -0,0 +1,37 @@
+//jshint esversion:6
+
+//console.log("hay");
+
+//adding express
+// if we use const we need to declare (//jshin esversion:6); esversion is current version of javascript.
+const express = require("express");
+
+const app = express();
+
+//specify what shoud happen browser touch wit server
+/*app.get("/",function(request, response){
+    // console.log(request);
+    // response.send("hello");
+    response.send("<h1>Hello World</h1>");
+});*/
+
+//short form
+app.get("/",function(req, res){
+    // console.log(req);
+    // response.send("hello");
+    res.send("<h1>Hello World</h1>");
+});
+
+app.get("/contact",function(req,res){
+    res.send("Contact me at: angela@gmail.com");
+});
+
+app.get("/about", function(req,res){
+    res.send("My name is Angela and I love beer and code.");
+});
+
+
+//use the app //add anonymous function
+app.listen(3000,function(){
+    console.log("Server started on port 3000");
+});
